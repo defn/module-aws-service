85d0834e246b94f06c2382ddb3b258d3e7cd4951
Tom Bombadil <amanibhavam@destructuring.org>
2016-07-03 22:42:35 -0700

    dont namespace vpc stuff, use Name tag

--- main.tf
+++ main.tf
@@ -54,7 +54,7 @@ output "subnet_ids" {
 }
 
 resource "aws_security_group" "sg" {
-  name = "${var.context_org}-${var.context_env}-${var.app_service_name}"
+  name = "${var.app_service_name}"
   description = "Service ${var.app_service_name}"
 
   vpc_id = "${data.terraform_remote_state.env.vpc_id}"
@@ -64,6 +64,7 @@ resource "aws_security_group" "sg" {
   }
 
   tags {
+    "Name" = "${var.context_org}-${var.context_env}-${var.app_service_name}-${element(data.terraform_remote_state.global.az_names,count.index)}"
     "App" = "${var.app_name}"
 		"Service" = "${var.app_service_name}"
     "Provisioner" = "tf"
